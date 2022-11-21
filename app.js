d500cd8bdc118585c6be3c53b57e00068b7b2411
Emily <emily.head9393@gmail.com>
2022-11-21 16:09:57 -0600

    uploading imageFile

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionsRouter = require('./routes/promotionsRouter')
 const partnerRouter = require('./routes/partnerRouter')
+const uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 const url = config.mongoUrl;
@@ -63,7 +64,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionsRouter);
 app.use('/partners', partnerRouter);
-
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
